cadafb195f016a9f3d0ec39f17b4062cf6cbc00d
CoderHF <54666674@qq.com>
2018-09-14 23:36:38 +0800

    Initial Commit

---
+++ HFWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HFWebImage
+//
+//  Created by CoderHF on 2018/9/14.
+//  Copyright © 2018年 CoderHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
